772d3615faa6f70ebac0e5deaad9c90403f04359
omar had <hadoussama@outlook.fr>
2021-09-05 04:13:30 +0200

    page asso en respensive

--- src/components/articlesAsso/AssoArt1.js
+++ src/components/articlesAsso/AssoArt1.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import FigureAsso from './FigureAsso';
 import AssoWrapper1 from './AssoWrapper1';
-
+import Intro from "../../components/Intro";
 const AssoArticle1 = () => {
     return (
         <div className='articleAsso Asso1'>
             <FigureAsso />
             <AssoWrapper1 />
+            <Intro />
         </div>
     );
 };

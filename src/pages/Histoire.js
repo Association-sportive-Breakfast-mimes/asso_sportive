1aeae502fce396bf81f2f5c8d9269bdacf202347
Tangram59 <animetangram202159@gmail.com>
2021-09-06 16:02:30 +0200

    habib

---
+++ src/pages/Histoire.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigations from '../components/Navigations';
+
+const Histoire = () => {
+    return (
+        <div>
+            <Navigations />
+            <h1>Histoire de l'Association et de l'Aikido</h1>
+        </div>
+    );
+};
+
+export default Histoire;

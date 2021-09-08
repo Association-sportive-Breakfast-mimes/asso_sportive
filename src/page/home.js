5fca47c911dcaf3e82fea5a721b5dda120ec2740
Apprenant <guillaumelesenne@gmail.com>
2021-09-08 15:35:29 +0200

    page accueil au top

--- src/page/home.js
+++ src/page/home.js
@@ -4,19 +4,27 @@ import Article3 from "../components/articles/Article3";
 import Article4 from "../components/articles/Article4";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Intersection from "../components/intersections/Intersection";
+import Intersectionhistory from "../components/intersections/Intersectionhistory";
+import Intersectionhoraire from "../components/intersections/Intersectionhoraire";
 import Intro from "../components/Intro";
 
 const home = () =>{
     return (
         <div className="home">
             <Header />
+                <Intersection />
             <div className="center">
                 <Intro />
                 <Article1 />
                 <Article2 />
                 <Article3 />
+            </div>
+            <Intersectionhistory />
+            <div className="center">
                 <Article4 />
             </div>
+            <Intersectionhoraire />
             <Footer />
             
             

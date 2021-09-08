2d53e905c9d6b9cbcbef9afbb61d8884681e04b8
Apprenant <guillaumelesenne@gmail.com>
2021-09-08 16:02:17 +0200

    page accueil au top2

--- src/components/intersections/Intersection.js
+++ src/components/intersections/Intersection.js
@@ -4,7 +4,7 @@ const Intersection = () => {
     return (
         <div className="inter">
             <div className="center">
-                <h1>L'association <span>ArEnVo</span> vous présente son association</h1>
+                <h1><span>Harenvo</span> vous présente son association</h1>
                 <p>En 1961, lors d’un voyage au pays du soleil levant, le Japon, j’ai rencontré Morihei UESHIBA le fondateur de l'aikido, Cela a été une chance car j’ai pu découvrir le monde de l’art martial. Lors de mon retour en France, je me suis lancé dans cette aventure qu’est l’apprentissage, le partage de connaissance …</p>
                 <a href="/Association">En savoir plus</a>
             </div>

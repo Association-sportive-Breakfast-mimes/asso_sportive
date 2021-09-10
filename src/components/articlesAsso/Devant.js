45eadd2b5cadde8331d242917f48ce51d320aff0
ApoCos <apolline.cossart@gmail.com>
2021-09-10 09:53:40 +0200

    ajout horaires

--- src/components/articlesAsso/Devant.js
+++ src/components/articlesAsso/Devant.js
@@ -9,25 +9,25 @@ export default function Devant ({perso}) {
 
   return (
     <div className="Devant">
-      <img src= {'http://localhost:1337' +perso.Photo} alt="photoPerso" />
-      <h2>{perso.Prenom + " " + perso.Nom}</h2>
-    </div>
-    // <Card className="Devant {classes.root}">
-    //   <CardActionArea>
-    //     <CardMedia
-    //       image= {perso.Photo !== null ? 'http://localhost:1337' +perso.Photo.formats.thumbnail.url : "..."}
-    //       title="photoPerso"
-    //     />
-    //     <CardContent>
-    //       <Typography 
-    //         gutterBottom variant="h5" 
-    //         component="h2"
-    //         className="NomPerso"
-    //       >
-    //         {perso.Prenom + " " + perso.Nom}
-    //       </Typography>
-    //     </CardContent>
-    //   </CardActionArea>
-    // </Card>
+    <img src={"http://localhost:1337/uploads/thumbnail_carapuce_17097a2dfa.jpeg"} alt="photoPerso" />
+    <h2>{perso.Prenom + " " + perso.Nom}</h2>
+  </div>
+  //   <Card className="Devant {classes.root}">
+  //     <CardActionArea>
+  //       <CardMedia
+  //         image={perso.Photo !== null ? 'http://localhost:1337' +perso.Photo.formats.thumbnail.url : "..."}
+  //         title="photoPerso"
+  //       />
+  //       <CardContent>
+  //         <Typography 
+  //           gutterBottom variant="h5" 
+  //           component="h2"
+  //           className="NomPerso"
+  //         >
+  //           {perso.Prenom + " " + perso.Nom}
+  //         </Typography>
+  //       </CardContent>
+  //     </CardActionArea>
+  //   </Card>
   );
 }
